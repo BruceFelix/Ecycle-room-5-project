fe807537a439e7a8be6ab95d928efd6b3f379506
paminus-kingori <pkmurungi.pm@gmail.com>
2021-08-02 14:27:57 +0300

    Updating user details

--- front-end/landing-shop/landing.php
+++ front-end/landing-shop/landing.php
@@ -7,7 +7,6 @@
     <title>ecycle | Landing page</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../styles/shop.css">
     <link rel="stylesheet" href="../../styles/landing.css">
 
